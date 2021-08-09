718bcaa97f4268147a8a6c1968cf802eefb75a4d
Frankazo <frankluisravlo@gmail.com>
2021-08-09 16:46:40 -0400

    Recreated Front end of Instagram navbar and header
    modified resources/views/layouts/app.blade.php and home.blade.php
    added custom font to resources/sass/app.scss and _variables.scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,25 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://avatars.githubusercontent.com/u/28962907?v=4" class="rounded-circle" alt="profile picture" style="max-height: 175px;">
         </div>
+        <div class="col-9 pt-5">
+            <div><h1>Frankazo</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>157</strong> posts</div>
+                <div class="pr-5"><strong>474</strong> followers</div>
+                <div class="pr-5"><strong>381</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">Frank Luis Ravelo </div>
+            <div>Best People are All Mad! <br />Tech, code, Travel...</div>
+            <div><a href="https://www.Frankluisravelo.com" target='_blank'>www.Frankluisravelo.com</a></div>
+        </div>
+ 
+        <div class="row">
+            <div class="col-4">
+<img src="https://instagram.fnyc1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.180.1440.1440a/s640x640/91891719_528605217709638_4142275141436633811_n.jpg?_nc_ht=instagram.fnyc1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=Rz7vb8FuxTgAX-NmheA&edm=ABfd0MgBAAAA&ccb=7-4&oh=f0141bfefcb0bbffe4098c70e8395528&oe=61181B04&_nc_sid=7bff83" alt="">            </div>
     </div>
 </div>
 @endsection
