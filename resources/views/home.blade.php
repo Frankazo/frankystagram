3754594f9bfcba0d30a098e09ba12a74543e09f9
Frankazo <frankluisravlo@gmail.com>
2021-08-11 12:18:41 -0400

    Updated registration process to include Username
    In resources/views/auth/register.blade.php added a form-group to contain the username field and handle error messages.
    In  app/Http/Controllers/Auth/RegisterController.php added the username field to the validator and create functions to validate agains the database and create the user respectively.
    In database/migrations updated the users table schema to contain the username as a unique value.
    In app/Models/User.php added the username field to the user model.
    
    Runned "php artisan migrate:refresh" to update database. and tested new fields with "php artisan tinker"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,15 +18,15 @@
             <div><a href="https://www.Frankluisravelo.com" target='_blank'>www.Frankluisravelo.com</a></div>
         </div>
  
-        <div class="row">
+        <div class="row pt-5">
             <div class="col-4">
-                <img src="https://images.unsplash.com/photo-1606787619248-f301830a5a57?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="pic">            
+                <img src="https://images.unsplash.com/photo-1606787619248-f301830a5a57?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="w-100">            
             </div>
             <div class="col-4">
-                <img src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="pic">            
+                <img src="https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="w-100">            
             </div>
             <div class="col-4">
-                <img src="https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="pic">            
+                <img src="https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="w-100">            
             </div>
         </div>
 </div>
