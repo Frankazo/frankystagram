f9e09d6a3c704429dcf08a14eb44d989708e293d
Frankazo <frankluisravlo@gmail.com>
2021-08-12 17:21:05 -0400

    EOD commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,18 @@
             <img src="https://avatars.githubusercontent.com/u/28962907?v=4" class="rounded-circle" alt="profile picture" style="max-height: 175px;">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>{{ $user->username }}</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="#">New Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>157</strong> posts</div>
                 <div class="pr-5"><strong>474</strong> followers</div>
                 <div class="pr-5"><strong>381</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">Frank Luis Ravelo </div>
-            <div>Best People are All Mad! <br />Tech, code, Travel...</div>
-            <div><a href="https://www.Frankluisravelo.com" target='_blank'>www.Frankluisravelo.com</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#" target='_blank'>{{ $user->profile->url }}</a></div>
         </div>
  
         <div class="row pt-5">
